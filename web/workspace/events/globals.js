a734820feda8743ff052e8b23db37b734312d802
David Longworth <dave@abovebored.co.uk>
2016-11-01 14:32:02 +0000

    Remove params array if empty

--- web/workspace/events/globals.js
+++ web/workspace/events/globals.js
@@ -1,5 +1,13 @@
 var Event = function (req, res, data, callback) {
 
+  // Remove params if blank
+  function noParams() { 
+   for (var x in data.params) { return false; }
+   return true;
+  }
+ 
+  if (noParams()) delete data.params;
+
   // Date and time to play with
   data.global.timestamp = new Date().getTime();
 
