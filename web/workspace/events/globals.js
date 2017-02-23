73a626936128ec0062bb46dcf673dd4a9adc5224
David Longworth <dave@abovebored.co.uk>
2017-02-23 13:13:40 +0000

    chore: remove ;

--- web/workspace/events/globals.js
+++ web/workspace/events/globals.js
@@ -2,27 +2,27 @@ var Event = function (req, res, data, callback) {
 
   // Remove params if blank
   function noParams() { 
-   for (var x in data.params) { return false; }
-   return true;
+   for (var x in data.params) { return false }
+   return true
   }
  
-  if (noParams()) delete data.params;
+  if (noParams()) delete data.params
 
   // Date and time to play with
-  data.global.timestamp = new Date().getTime();
+  data.global.timestamp = new Date().getTime()
 
   // Url stuff
-  data.global.protocol = req.protocol;
+  data.global.protocol = req.protocol
 
   // Override page meta description (latest section)
   if (data.params.article && data.latest && data.latest.results) {
-    data.page.description = data.latest.results[0].attributes.meta;
+    data.page.description = data.latest.results[0].attributes.meta
   }
  
   // Fin
-  callback();
+  callback()
 }
 
 module.exports = function (req, res, data, callback) {
   return new Event(req, res, data, callback)
-};
+}
