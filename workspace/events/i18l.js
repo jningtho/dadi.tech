53d7db02a9c07030c036f126e64106edf80bec89
David Longworth <dave@abovebored.co.uk>
2017-05-04 13:55:22 +0100

    fix: redirect if lang not found

--- workspace/events/i18l.js
+++ workspace/events/i18l.js
@@ -10,14 +10,18 @@ var Event = function (req, res, data, callback) {
   // Otherwise redirect to the browser default (fallback to 'en')
   else {
     lang = langParser.parse(req.headers['accept-language'])[0].code ? langParser.parse(req.headers['accept-language'])[0].code : 'en'
-
-    res.writeHead(302, { Location: '/' + lang });
-    return res.end();
+    res.writeHead(302, { Location: '/' + lang })
+    return res.end()
   }
 
   // Load the language file (ignore english)
-  data.i18l = lang && lang != 'en' ? require(path.join(__dirname, '/../lang/', data.params.lang + '.json')) : null
-  
+  try {
+    data.i18l = lang && lang != 'en' ? require(path.join(__dirname, '/../lang/', data.params.lang + '.json')) : null
+  } catch(e) {
+    res.writeHead(302, { Location: '/en' })
+    return res.end()
+  }
+
   // Fin
   callback()
 }
