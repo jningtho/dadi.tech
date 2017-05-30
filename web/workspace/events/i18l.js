041b80903b13072b1b58d4f1105baa423a84860e
David Longworth <dave@abovebored.co.uk>
2017-05-30 16:35:35 +0100

    feat: lang tweaks

--- web/workspace/events/i18l.js
+++ web/workspace/events/i18l.js
@@ -2,24 +2,27 @@ var path = require('path')
 var langParser = require('accept-language-parser')
 var lang
 
+// Set the primary language
+var primaryLang = 'en'
+
 var Event = function (req, res, data, callback) {
   // If there's a lang variable passed from pages
   if (data.params && data.params.lang) {
     lang = data.params.lang
   }
 
-  // Otherwise redirect to the browser default (fallback to 'en')
+  // Otherwise redirect to the browser default (fallback to primaryLang)
   else {
-    lang = langParser.parse(req.headers['accept-language'])[0].code ? langParser.parse(req.headers['accept-language'])[0].code : 'en'
+    lang = req.headers['accept-language'] ? langParser.parse(req.headers['accept-language'])[0].code : primaryLang
     res.writeHead(302, { Location: '/' + lang })
     return res.end()
   }
 
   // Load the language file (ignore english)
   try {
-    data.i18l = lang && lang != 'en' ? require(path.join(__dirname, '/../lang/', lang + '.json')) : null
+    data.i18l = lang && lang != primaryLang ? require(path.join(__dirname, '/../lang/', lang + '.json')) : null
   } catch(e) {
-    res.writeHead(302, { Location: '/en' })
+    res.writeHead(302, { Location: '/' + primaryLang })
     return res.end()
   }
 
