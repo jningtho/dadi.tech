85801c2837a61e40b0b93615731603c6eafec5ec
mingard <info@arthurmingard.com>
2017-07-13 18:36:33 +0100

    feat: import all static into local DB

--- api/scripts/import-static.js
+++ api/scripts/import-static.js
@@ -1,14 +1,59 @@
 const config = require('@dadi/api').Config
+const DadiAPI = require('@dadi/api-wrapper')
 const fs = require('fs')
+const path = require('path')
+
+const api = new DadiAPI({
+  uri: config.get('publicUrl.host'),
+  port: config.get('server.port'),
+  credentials: {
+    clientId: 'testClient',
+    secret: 'superSecret'
+  },
+  version: config.get('query.useApiVersion'),
+  database: config.get('database.database')
+})
 
 const Import = function () {
-  this.inject('concepts')
-  this.inject('support')
-  this.inject('products')
+  this.clear('concepts')
+    .then(res => {
+      this.inject('concepts')
+    })
+  this.clear('support')
+    .then(res => {
+      this.inject('support')
+    })
+  this.clear('products')
+    .then(res => {
+      this.inject('products')
+    })
 }
 
-Import.prototype.inject = function (fileName) {
-  
+Import.prototype.clear = function (collection) {
+  return api.in(collection)
+      .whereFieldExists('title') // Temporary pending: https://github.com/dadi/api-wrapper-core/issues/12
+      .delete()
+}
+
+Import.prototype.inject = function (collection) {
+  const data = fs.readFileSync(path.join(__dirname, `/static/${collection}.json`), {encoding: 'utf8'})
+
+  if (typeof data !== 'string') throw Error('file data must return string')
+
+  const json = JSON.parse(data)
+  if (!json.data) throw Error('Missing data block in static JSON')
+
+  console.log(`Inserting ${json.data.length} documents into ${collection}`)
+  json.data.map(node => {
+    api.in(collection)
+      .create(node)
+      .then()
+      .catch(e => {
+        console.log(e.error)
+      })
+  })
+  // api.in('collection')
+      // .create(node)
 }
 
 module.exports = new Import()
