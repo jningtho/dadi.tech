ed528567cb1055f61c5f32803377ab5b1f02a8f5
David Longworth <dave@abovebored.co.uk>
2017-08-14 16:35:18 +0100

    feat: update langage event

--- web/workspace/events/i18l.js
+++ web/workspace/events/i18l.js
@@ -5,19 +5,32 @@ var path = require('path')
 var primaryLang = 'en'
 
 var Event = function (req, res, data, callback) {
+  // Path without trailing slash & default lang
   var toPath = url.parse(req.url, true).pathname.replace(/\/+$/, '')
 
-  // Redirect if no lang
-  if (data.params && data.params.lang && data.params.lang == primaryLang || ~(toPath.indexOf('assets')) || ~(toPath.indexOf('en'))) {
-    // Nothing
-  }
-  else {
-    res.writeHead(302, { Location: '/' + primaryLang + toPath })
-    return res.end()
+  // Remove data.params.lang
+  if (data.params && data.params.lang && data.params.lang) {
+    var rmvLang = new RegExp('^\/' + data.params.lang, 'gmi')
+    toPath = toPath.replace(rmvLang, '')
   }
 
-  // Fin
-  callback()
+  // Do nothing if default language
+  if (data.params && data.params.lang && data.params.lang === primaryLang) {
+    callback()
+  }
+
+  // See if this is a language we speak
+  else {
+    try {
+      data.i18l = require(path.join(__dirname,'/../lang/', data.params.lang + '.json'))
+    } catch (err) {
+      // Redirect to default language
+      res.writeHead(302, { Location: '/' + primaryLang + toPath })
+      return res.end()
+    }
+
+    callback()
+  }
 }
 
 module.exports = function (req, res, data, callback) {
