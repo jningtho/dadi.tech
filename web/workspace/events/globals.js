2310e2a48a6e7b8fb4357e2e0606c958714b126f
David Longworth <dave@abovebored.co.uk>
2017-08-27 13:00:41 +0100

    fix: lang redirect

--- web/workspace/events/globals.js
+++ web/workspace/events/globals.js
@@ -1,12 +1,12 @@
 var Event = function (req, res, data, callback) {
 
   // Remove params if blank
-  function noParams() { 
-   for (var x in data.params) { return false }
-   return true
-  }
+  // function noParams() { 
+  //  for (var x in data.params) { return false }
+  //  return true
+  // }
  
-  if (noParams()) delete data.params
+  // if (noParams()) delete data.params
 
   // Date and time to play with
   data.global.timestamp = new Date().getTime()
