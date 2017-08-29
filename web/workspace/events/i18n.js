50ec2cf768e717d27f0c661f795ea8f67ba620f6
David Longworth <dave@abovebored.co.uk>
2017-08-29 15:50:10 +0100

    feat: RU content

--- web/workspace/events/i18n.js
+++ web/workspace/events/i18n.js
@@ -5,7 +5,7 @@ var path = require('path')
 var primaryLang = 'en'
 
 // Set what we support
-var supportedLangs = ['en','de','jp','fr','es']
+var supportedLangs = ['en','de','jp','fr','es','ru']
 
 var Event = function (req, res, data, callback) {
   // Path without trailing slash & default lang
