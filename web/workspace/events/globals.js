35e1b2933350095903e4faca568a17dc28e2b992
David Longworth <dave@abovebored.co.uk>
2017-10-11 13:38:37 +0100

    feat: seo updates

--- web/workspace/events/globals.js
+++ web/workspace/events/globals.js
@@ -1,20 +1,21 @@
 var Event = function (req, res, data, callback) {
-
-  // Remove params if blank
-  // function noParams() { 
-  //  for (var x in data.params) { return false }
-  //  return true
-  // }
- 
-  // if (noParams()) delete data.params
-
   // Date and time to play with
   data.global.timestamp = new Date().getTime()
 
   // Override page meta description
   // Latest section
   if (data.params && data.params.article && data.latest && data.latest.results) {
-    data.page.description = data.latest.results[0].attributes.meta
+    data.page.description = data.latest.results[0].attributes.meta || data.latest.results[0].attributes.preface
+  }
+
+  // Success stories
+  if (data.params && data.params.client && data.successes && data.successes.results) {
+    data.page.description = data.successes.results[0].summary
+  }
+
+  // Concepts
+  if (data.params && data.params.concept && data.concepts && data.concepts.results) {
+    data.page.description = data.concepts.results[0].overview
   }
 
   // Microservices
