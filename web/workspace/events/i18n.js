a9164cd0edcc6c26b56889dd2f7557a88170b344
David Longworth <dave@abovebored.co.uk>
2017-10-18 11:55:29 +0100

    fix: product redirects

--- web/workspace/events/i18n.js
+++ web/workspace/events/i18n.js
@@ -6,20 +6,23 @@ var primaryLang = 'en'
 
 // Set what we support
 var supportedLangs = ['en','de','jp','fr','es','ru','pt','zh_hant','zh_hans']
+var bypassUrls = []
 
 var Event = function (req, res, data, callback) {
   // Path without trailing slash & default lang
   var toPath = url.parse(req.url, true).pathname.replace(/\/+$/, '')
+  var firstFolder = toPath.replace(/^\/([^\/]*).*$/, '$1')
 
-  // Remove data.params.lang
-  if (data.params && data.params.lang && data.params.lang) {
-    var rmvLang = new RegExp('^\/' + data.params.lang, 'gmi')
+  // Remove firstFolder
+  if (~(supportedLangs.indexOf(firstFolder))) {
+    var rmvLang = new RegExp('^\/' + firstFolder, 'gmi')
     toPath = toPath.replace(rmvLang, '')
-    data.pathNoLang = toPath
   }
 
+  data.pathNoLang = toPath  
+
   // Do nothing if default language
-  if (!~(supportedLangs.indexOf(data.params.lang)) || !data.params.lang || !data.params) { 
+  if (!~(supportedLangs.indexOf(firstFolder)) && !~(bypassUrls.indexOf(toPath))) { 
     res.writeHead(302, { Location: '/' + primaryLang + toPath })
     return res.end()
   }
